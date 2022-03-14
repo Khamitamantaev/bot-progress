8ae27c732f8c2c77a8ff12c3ed81a8bcc7c651f6
Khamitamantaev <32989550+Khamitamantaev@users.noreply.github.com>
2022-03-14 23:42:37 +0300

    simple reenter working fine

--- src/progress-module/scenes/random-number.scene.ts
+++ src/progress-module/scenes/random-number.scene.ts
@@ -1,14 +1,38 @@
-import { Scene, SceneEnter, SceneLeave, Command } from 'nestjs-telegraf';
+import { Scene, SceneEnter, SceneLeave, Command, Ctx, Action } from 'nestjs-telegraf';
+import { SceneContext } from 'telegraf/typings/scenes';
+import { Update } from 'telegraf/typings/core/types/typegram';
 import { HELLO_SCENE_ID } from '../../app.contants';
 import { Context } from '../../interfaces/context.interface';
 
 @Scene(HELLO_SCENE_ID)
 export class RandomNumberScene {
-  @SceneEnter()
-  onSceneEnter(): string {
-    console.log('Enter to scene');
-    return 'Welcome on scene ✋';
-  }
+    @SceneEnter()
+    async enter(@Ctx() context: SceneContext) {
+        context.reply('2+2 = ?', {
+            reply_markup: {
+                inline_keyboard: [
+                    [{text: 'Может быть 4?', callback_data: '4'}],
+                    [{text: 'Точно пять!', callback_data: '5'}],
+                ],
+            },
+        });
+    }
+
+    @Action(/4|5/)
+    async onAnswer(
+      @Ctx() context: SceneContext & {update: Update.CallbackQueryUpdate}
+    ) {
+        const cbQuery = context.update.callback_query;
+        const userAnswer = 'data' in cbQuery ? cbQuery.data : null;
+ 
+        if (userAnswer === '4') {
+            context.reply('верно!');
+            context.scene.enter('nextSceneId');
+        } else {
+            context.reply('подумай еще');
+            context.scene.reenter()
+        }
+    }
 
   @SceneLeave()
   onSceneLeave(): string {
