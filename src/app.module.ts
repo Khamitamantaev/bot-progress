53726ced4b10f07d37156b8eea0d3815a6f875ee
Khamitamantaev <32989550+Khamitamantaev@users.noreply.github.com>
2022-03-15 21:15:46 +0300

    not now

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,7 +12,7 @@ import { ProgressModule } from './progress-module/progress-module';
     ConfigModule.forRoot(),
     TelegrafModule.forRootAsync({
       useFactory: (configService: ConfigService) => ({
-        token: process.env.TELEGRAM_API_KEY,
+        token: configService.get('TELEGRAM_API_KEY'),
         middlewares: [sessionMiddleware],
         include: [ProgressModule]
       }),
